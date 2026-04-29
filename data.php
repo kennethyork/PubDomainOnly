7600b0e66bc4376eed3380c79bb8242b3db26089
kenny <kenneth@localhost>
2026-04-29 16:52:47 -0400

    Remove verse numbers from Amazing Grace lyrics

--- data.php
+++ data.php
@@ -19036,31 +19036,26 @@ Or wilt thou, ere this very day be done,
 I once was lost, but now am found,
   Was blind, but now I see.
 
-2
 'Twas grace that taught my heart to fear,
   And grace my fears relieved;
 How precious did that grace appear
   The hour I first believed!
 
-3
 Through many dangers, toils, and snares
   I have already come;
 'Tis grace has brought me safe thus far,
   And grace will lead me home.
 
-4
 The Lord has promised good to me,
   His word my hope secures;
 He will my shield and portion be
   As long as life endures.
 
-5
 Yes, when this flesh and heart shall fail,
   And mortal life shall cease,
 I shall possess, within the veil,
   A life of joy and peace.
 
-6
 The earth shall soon dissolve like snow,
   The sun forbear to shine;
 But God, who call'd me here below,
